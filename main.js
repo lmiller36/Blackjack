effc559ba1477ad4b798dc1a514a302e85361bb1
Lorne Miller <lmiller36@wisc.edu>
2020-03-19 23:31:53 -0500

    Playable blackjack with starting money

--- main.js
+++ main.js
@@ -35,6 +35,7 @@ class Card {
     getSVG() {
         var img = document.createElement('img');
         img.src = "./svg-cards/" + this.faceValue + "_of_" + this.suit + ".svg";
+        img.width = "125";
         return img;
     }
 }
@@ -101,7 +102,7 @@ function addCards(cards) {
     })
 
     var final = Array.from(set);
-    final.sort((a,b)=> {return b-a});
+    final.sort((a, b) => { return b - a });
 
     // didn't bust
     if (final.length > 0)
@@ -114,7 +115,6 @@ function addCards(cards) {
 
 // returns a shuffled array of cards 
 class Deck {
-
     constructor() {
         this.cards = [];
         var suits = Object.keys(document.suits);
@@ -128,73 +128,361 @@ class Deck {
         }
 
         this.cards = shuffle(this.cards);
+
+        // this.cards.splice(0, 1, new Card("clubs", "10"));
+        // this.cards.splice(2, 1, new Card("clubs", "ace"));
+        // // this.cards.splice(4, 1, new Card("clubs", "ace"));
+        // this.cards.splice(5, 1, new Card("clubs", "ace"));
+        // this.cards.splice(7, 1, new Card("clubs", "ace"));
+        // this.cards.splice(8, 1, new Card("clubs", "ace"));
+
+
     }
 }
 
 var currentCard = 0;
 var playersCards = [];
 var dealerCards = [];
+var currentBets = [];
 var hasBusted = false;
+var gameOver = false;
+var doubled = false;
 var deck;
+var sum = -1;
+var numHands = 0;
+var dealerSum = -1;
+document.chips = 100;
+document.bet = 1;
+var currHand = 0;
+
+function changeBet(betChange) {
+    var newBet = document.bet + betChange;
+    if (newBet > 0 && newBet <= document.chips)
+        document.bet = newBet;
+    document.getElementById("bet").innerText = "Bet: " + document.bet;
+}
+
+function newGame() {
+    currentCard = 0;
+    playersCards = [[]];
+    dealerCards = [];
+    currentBets = [document.bet];
+    hasBusted = false;
+    gameOver = false;
+    doubled = false;
+    currHand = 0;
+    numHands = 0;
+    sum = -1;
+    dealerSum = -1;
+
+    deck = new Deck();
+
+    // remove previous game's cards
+    removeChildren("dealerContainer");
+    removeChildren("playerHandsContainer");
+
+    document.getElementById("dealerSumContainer").style.display = "none";
+    document.getElementById("dealerSumContainer").style.display = "none";
+
+    document.getElementById("actionContainer_inGame")
+        .querySelector(".double").style.display = "";
+
+    toggleButtons(true);
+
+    // initialize player hand
+    addPlayerHand();
+
+    // initial cards
+    addDealerCard(false, false);
+    addPlayerCard("hand_0");
+    addDealerCard(true, false);
+    addPlayerCard("hand_0");
+
+    // if dealer has 21, game is automatically over
+    console.log(dealerCards[1].faceValue);
+    console.log(dealerCards[0].value);
+    if(dealerCards[1].faceValue == "ace" && dealerCards[0].value == 10){
+        hideButtons();
+        setTimeout(stand, 1000);
+    }
+
+    document.getElementById("hand_0").querySelector(".cards").className += " currentHand";
+
+    if (playersCards[0].length == 2 && playersCards[0][0].value == playersCards[0][1].value) {
+        var buttons = document.querySelector('#actionContainer_inGame');
+        buttons.querySelector('.split').style.display = "";
+    }
+    else {
+        var buttons = document.querySelector('#actionContainer_inGame');
+        buttons.querySelector('.split').style.display = "none";
+    }
+
+    // if dealer has 21 w/ an ace showing, round is automatically over
+    // if()
+
+    // initializeML();
+}
+
+function removeChildren(nodeStr) {
+    var node = document.getElementById(nodeStr);
+    var cNode = node.cloneNode(false);
+    node.parentNode.replaceChild(cNode, node);
+}
+
+function double() {
+    if (hasBusted || gameOver) return
+    doubled = true;
+    currentBets[currHand] *= 2;
+    // document.bet *= 2;
+    document.getElementById("hand_" + currHand).querySelector(".textDiv")
+        .querySelector(".bet").querySelector(".betText").innerText = "Bet: " + currentBets[currHand];
+    // document.getElementById("bet").innerText = "Bet: " + document.bet;
+
+    hit();
+    stand();
+}
+
+function hideButtons(){
+    document.getElementById("actionContainer_inGame")
+    .style.display = "none";
+}
 
 function hit() {
-    if (!hasBusted)
-        addPlayerCard();
+    if (hasBusted || gameOver) return;
+
+    hideButtons();
+
+    addPlayerCard("hand_" + currHand);
 }
 
 function delayOneSecond() {
     setTimeout(function () { }, 1000);
 }
 
-function stand() {
-    // flip over hidden card
-    var hidden = document.getElementById("hiddenCard");
-    hidden.style.display = "none";
-    var dealersCards = document.getElementById("dealerContainer")
-    var firstCard = deck.cards[0];
-    dealersCards.insertBefore(firstCard.getSVG(), dealersCards.firstChild);
+function toggleButtons(inGame) {
 
-    // show dealer sum
-    var dealerSumDiv = document.getElementById("dealerSumContainer");
-    dealerSumDiv.style.display = "";
+    if (inGame) {
+        document.getElementById("actionContainer_replay").style.display = "none";
+        document.getElementById("actionContainer_inGame").style.display = "";
+        document.getElementById("betButtons").style.display = "none";
 
-    var sums = updateSum(dealerCards, "dealerSumText")
-
-    // finish out dealer cards
-    var maxSum = sums[0];
-    console.log(maxSum);
-    var bust = (maxSum == "bust");
-    var mustStop = maxSum >= 17;
-
-    delayOneSecond();
-
-    // dealer will continue playing until they bust
-    // or reach a value of 17
-
-    var addCardInterval;
-    var addCard = () => {
-        sums = addDealerCard(true, true);
-        maxSum = sums[0];
-        console.log(maxSum)
-        if (maxSum == "bust") bust = true;
-        else {
-            if (maxSum >= 17) mustStop = true;
-        }
-
-        if (bust || mustStop) clearInterval(addCardInterval);
     }
-    
-    // start autoplay for dealer, playing one card / sec
-    // only initiate if dealer starts below 17
-    if (!bust && !mustStop)
-        addCardInterval = setInterval(addCard, 1000);
+    else {
+        document.getElementById("actionContainer_replay").style.display = "";
+        document.getElementById("actionContainer_inGame").style.display = "none";
+        document.getElementById("betButtons").style.display = "";
+    }
 }
 
-function updateSum(cards, div) {
+function endGame() {
+    toggleButtons(false);
+
+    // determine bets
+    var i = 0;
+    playersCards.forEach(hand => {
+        var sum = addCards(hand)[0];
+        var cards = document.getElementById("hand_" + i).querySelector(".cards");
+
+        // subtract if lost
+        if (sum == -1 || sum < dealerSum) {
+            document.chips -= currentBets[i];
+            cards.className += " loss";
+        }
+        else if (sum > dealerSum) {
+            if (sum == 21 && hand.length == 2) {
+                var bjBet = Math.round(currentBets[i] * 1.5);
+                document.getElementById("hand_" + (currHand - 1)).querySelector(".textDiv")
+                    .querySelector(".bet").querySelector(".betText").innerText = "Bet: " + bjBet;
+                document.chips += bjBet;
+            }
+            else
+                document.chips += currentBets[i];
+
+            cards.className += " win";
+        }
+        i++;
+    })
+
+
+
+    //update chips
+    document.getElementById("chips").innerText = "Chips: " + document.chips;
+
+}
+
+function addPlayerHand() {
+    var handsContainer = document.querySelector("#playerHandsContainer");
+    var handDiv = document.createElement("div");
+    handDiv.id = "hand_" + handsContainer.childNodes.length;
+    handDiv.className = "centerContent";
+    handDiv.style.display = "grid";
+
+    var cardsDiv = document.createElement("div");
+    cardsDiv.className = "cards";
+
+    // <ul>
+    //     <li>
+    //         <span class="label">Blah blah</span>
+    //         5
+    //     </li>
+    // </ul>
+
+    //    var list = document.createElement("ul");
+    //    var listElem = document.createElement("span");
+    //    listElem.className = "sum label";
+    //    listElem.innerText = "Sum: 13";
+    //    listElem.innerHTML += "Bet:20";
+
+    //    list.appendChild(listElem);
+
+    var sumDiv = document.createElement("div");
+    sumDiv.className = "sum";
+    var sumText = document.createElement("p");
+    sumText.className = "sumText text";
+    sumText.innerText = "Sum:";
+    sumText.style.marginRight = "80px";
+    sumDiv.appendChild(sumText);
+
+    var betDiv = document.createElement("div");
+    betDiv.className = "bet";
+    var betText = document.createElement("p");
+    betText.className = "betText text";
+    betText.innerText = "Bet:1";
+    betDiv.appendChild(betText);
+
+    var textDiv = document.createElement("div");
+    textDiv.className = "textDiv";
+    textDiv.style.display = "inline-flex";
+    textDiv.appendChild(sumDiv);
+    textDiv.appendChild(betDiv);
+
+    handDiv.appendChild(cardsDiv);
+    // handDiv.appendChild(list);
+    handDiv.appendChild(textDiv);
+
+    // handDiv.appendChild(sumDiv);
+    // handDiv.appendChild(betDiv);
+
+    handsContainer.appendChild(handDiv);
+    numHands++;
+
+    return handDiv;
+}
+
+function split() {
+
+    playersCards.push([]);
+    currentBets.push(document.bet);
+
+    // denote that player has an additional hand to play out
+    var currentHand = document.getElementById("hand_" + currHand);
+    var createdHand = addPlayerHand();
+
+    var newHandNum = createdHand.id.split("_")[1];
+
+    var cardToMove = currentHand.querySelector(".cards").childNodes[1];
+    currentHand.querySelector(".cards").removeChild(cardToMove);
+    createdHand.querySelector(".cards").appendChild(cardToMove);
+
+    playersCards[newHandNum].push(playersCards[currHand].splice(1, 1)[0]);
+
+    addPlayerCard(currentHand.id);
+    addPlayerCard(createdHand.id);
+
+    if (playersCards[currHand].length == 2 && playersCards[currHand][0].value == playersCards[currHand][1].value) {
+        var buttons = document.querySelector('#actionContainer_inGame');
+        buttons.querySelector('.split').style.display = "";
+    }
+    else {
+        var buttons = document.querySelector('#actionContainer_inGame');
+        buttons.querySelector('.split').style.display = "none";
+    }
+}
+
+function stand() {
+    //if (hasBusted || gameOver) return;
+
+    currHand++;
+
+    document.getElementById("actionContainer_inGame")
+        .querySelector(".double")
+        .style.display = "";
+
+    if (currHand < playersCards.length) {
+        document.getElementById("hand_" + (currHand - 1)).querySelector(".cards").classList.remove("currentHand");
+
+        document.getElementById("hand_" + currHand).querySelector(".cards").className += " currentHand";
+
+        if (playersCards[currHand].length == 2 && playersCards[currHand][0].value == playersCards[currHand][1].value) {
+            var buttons = document.querySelector('#actionContainer_inGame');
+            buttons.querySelector('.split').style.display = "";
+        }
+        else {
+            var buttons = document.querySelector('#actionContainer_inGame');
+            buttons.querySelector('.split').style.display = "none";
+        }
+    }
+    // if round is over
+    else if (currHand == playersCards.length) {
+        document.getElementById("actionContainer_inGame").style.display = "none";
+        document.getElementById("hand_" + (currHand - 1)).querySelector(".cards").classList.remove("currentHand");
+
+        // flip over hidden card
+        var hidden = document.getElementById("hiddenCard");
+        hidden.style.display = "none";
+        var dealersCards = document.getElementById("dealerContainer")
+        var firstCard = deck.cards[0];
+        dealersCards.insertBefore(firstCard.getSVG(), dealersCards.firstChild);
+
+        // show dealer sum
+        var dealerSumDiv = document.getElementById("dealerSumContainer");
+        dealerSumDiv.style.display = "";
+
+        var sums = updateSum(dealerCards, document.getElementById("dealerSumText"))
+
+        // finish out dealer cards
+        var maxSum = sums[0];
+
+        var bust = (maxSum == "bust");
+        var mustStop = maxSum >= 17;
+        dealerSum = maxSum;
+        delayOneSecond();
+
+        // dealer will continue playing until they bust
+        // or reach a value of 17
+
+        var addCardInterval;
+        var addCard = () => {
+            sums = addDealerCard(true, true);
+            maxSum = sums[0];
+            dealerSum = maxSum;
+
+            if (maxSum == "bust") {
+                bust = true;
+                dealerSum = -1;
+            }
+            else {
+                if (maxSum >= 17) mustStop = true;
+            }
+
+            if (bust || mustStop) {
+                clearInterval(addCardInterval);
+                endGame();
+            }
+        }
+
+        // start autoplay for dealer, playing one card / sec
+        // only initiate if dealer starts below 17
+        if (!bust && !mustStop)
+            addCardInterval = setInterval(addCard, 1000);
+        else
+            endGame();
+    }
+}
+
+function updateSum(cards, sumText, bet) {
     var sums = addCards(cards);
-    console.log(sums);
     var str = "Sum: " + sums[0];
-    var sumText = document.getElementById(div);
 
     for (var i = 1; i < sums.length; i++)
         str += " or " + sums[i];
@@ -212,7 +500,7 @@ function addDealerCard(showCard, updateSums) {
     if (!showCard) {
         img = document.createElement('img');
         img.src = "./svg-cards/Card_back.svg";
-        img.width = "222";
+        img.width = "125";
         img.id = "hiddenCard";
     }
     else {
@@ -224,44 +512,44 @@ function addDealerCard(showCard, updateSums) {
 
     var sums;
     if (updateSums)
-        sums = updateSum(dealerCards, "dealerSumText")
-
+        sums = updateSum(dealerCards, document.getElementById("dealerSumText"))
 
     currentCard++;
 
     return sums;
 }
 
-function addPlayerCard() {
-    var playersCardsDiv = document.getElementById("playerContainer")
+function updateBet(newBet) {
+    document.bet = newBet;
+    document.getElementById("bet").innerText = "Bet: " + document.bet;
+}
+
+function addPlayerCard(handStr) {
+    var handNum = handStr.split("_")[1];
+
+    var playersCardsDiv = document.getElementById(handStr).querySelector(".cards");
 
     // add card to window
     var card = deck.cards[currentCard];
     playersCardsDiv.appendChild(card.getSVG());
 
-    playersCards.push(card);
+    playersCards[handNum].push(card);
 
     // update sum
-    var sums = updateSum(playersCards, "playerSumText");
+    var sums = updateSum(playersCards[handNum],
+        document.getElementById(handStr)
+            .querySelector(".textDiv")
+            .querySelector(".sum")
+            .querySelector(".sumText"),
+        currentBets[handNum]);
+    sum = sums[0];
 
     // if busted, update variable
-    if (sums[0] == "bust") {
-        hasBusted = true;
+    if (sums[0] == "bust" || sums[0] == 21) {
+        console.log("timeout")
         setTimeout(stand, 1000);
     }
 
-
     // increment used card
     currentCard++;
-}
-
-function start(){
-     deck = new Deck();
-
-    // initial cards
-    addDealerCard(false, false);
-    addPlayerCard();
-    addDealerCard(true, false);
-    addPlayerCard();
-
 }
